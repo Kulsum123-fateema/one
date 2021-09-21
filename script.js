4aeb16e8fe36effa970b4cd6f258e1c21246b564
Kulsum123-fateema <85397197+Kulsum123-fateema@users.noreply.github.com>
2021-09-21 13:27:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+function playPause1(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "1976";
+    } else {
+        vid.pause();
+        btn.innerHTML = "1976";
+    }
+}
+
+function playPause2(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "1993";
+    } else {
+        vid.pause();
+        btn.innerHTML = "1993";
+    }
+}
+
+function playPause3(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "2008";
+    } else {
+        vid.pause();
+        btn.innerHTML = "2008";
+    }
+}
+
+function playPause4(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "2008";
+    } else {
+        vid.pause();
+        btn.innerHTML = "2008";
+    }
+}
+
+function playPause5(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "123";
+    } else {
+        vid.pause();
+        btn.innerHTML = "123";
+    }
+}
+
+function playPause6(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "123";
+    } else {
+        vid.pause();
+        btn.innerHTML = "123";
+    }
+}
+function playPause7(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "2018";
+    } else {
+        vid.pause();
+        btn.innerHTML = "2018";
+    }
+}
+function playPause8(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "123";
+    } else {
+        vid.pause();
+        btn.innerHTML = "123";
+    }
+}
+function playPause9(btn, vid) {
+    var vid = document.getElementById(vid);
+    if (vid.paused) {
+        vid.play();
+        btn.innerHTML = "2020";
+    } else {
+        vid.pause();
+        btn.innerHTML = "2020";
+    }
+}
